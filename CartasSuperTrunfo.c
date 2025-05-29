73d4ec4778cbf109045e5dc58ef294000c5edfd8
Telo021 <marceloxl456@gmail.com>
2025-05-29 23:21:31 +0000

    fim

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@ int main() {
     char Estado1;
     char codigo1[4];
     char cidade1[20];
-    int populacao1;
+    unsigned long int populacao1;
     float km1;
     float pib1;
     int turistico1;
@@ -13,13 +13,14 @@ int main() {
     char Estado2;
     char codigo2[4];
     char cidade2[20];
-    int populacao2;
+    unsigned long int populacao2;
     float km2;
     float pib2;
     int turistico2;
 
     float densidade1, densidade2;
     float pibper1, pibper2;
+    float superpoder1, superpoder2;
 
 
 
@@ -32,7 +33,7 @@ int main() {
     printf("Nome da Cidade: ");
     scanf(" %[^\n]s", &cidade1); //para funcionar com espaço: %[^\n]s
     printf("População: ");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
     printf("Área (em km²): ");
     scanf("%f", &km1);
     printf("PIB: ");
@@ -48,7 +49,7 @@ int main() {
     printf("Nome da Cidade: ");
     scanf(" %[^\n]s", &cidade2); //para funcionar com espaço: %[^\n]s
     printf("População: ");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
     printf("Área (em km²): ");
     scanf("%f", &km2);
     printf("PIB: ");
@@ -64,6 +65,8 @@ int main() {
     pibper1 = (float)(pib1* 1000000000) / populacao1;     // Multiplicar para 1 Bilhão para  funcionar nas casas Decimais
     pibper2 = (float)(pib2* 1000000000) / populacao2;
 
+    superpoder1 = populacao1 + km1 + pib1 + turistico1 + pibper1 + (1 / densidade1);
+    superpoder2 = populacao2 + km2 + pib2 + turistico2 + pibper2 + (1 / densidade2);
 
     //Exibição da Carta 1
 
@@ -72,12 +75,13 @@ int main() {
     printf("Estado: %c \n",Estado1);
     printf("Código: %s \n",codigo1);
     printf("Nome da Cidade: %s \n",cidade1);
-    printf("População: %d \n",populacao1);
+    printf("População: %lu \n",populacao1);
     printf("Área: %.2f km²\n",km1);             // %.2f para 2 cassas decimais
     printf("PIB: %.2f Bilhoes de Reais\n",pib1);
     printf("Pontos Turísticos: %d \n",turistico1);
     printf("Densidade Populacional: %.2f \n", densidade1);
     printf("PIB per Capita: %.2f \n", pibper1);
+    printf("Superpoder: %.2f\n", superpoder1);
 
     //Exibição da Carta 2
 
@@ -86,12 +90,25 @@ int main() {
     printf("Estado: %c \n",Estado2);
     printf("Código: %s \n",codigo2);
     printf("Nome da Cidade: %s \n",cidade2);
-    printf("População: %d \n",populacao2);
+    printf("População: %lu \n",populacao2);
     printf("Área %.2f km²\n",km2);            // %.2f para 2 cassas decimais
     printf("PIB: %.2f Bilhoes de Reais\n",pib2);
     printf("Pontos Turísticos: %d \n",turistico2);
     printf("Densidade Populacional: %.2f \n", densidade2);
     printf("PIB per Capita: %.2f \n", pibper2);
+    printf("Superpoder: %.2f\n", superpoder2);
+
+    //Conparação das Cartas
+
+    printf("\n==== Resultados das Comparações ====\n");
+    
+    printf("População: %d\n", populacao1 > populacao2);
+    printf("Área: %d\n", km1 > km2);
+    printf("PIB: %d\n", pib1 > pib2);
+    printf("Pontos Turísticos: %d\n", turistico1 > turistico2);
+    printf("Densidade Populacional: %d\n", densidade1 < densidade2);
+    printf("PIB per Capita: %d\n", pibper1 > pibper2);
+    printf("Superpoder: %d\n", superpoder1 > superpoder2);
 
 
 
